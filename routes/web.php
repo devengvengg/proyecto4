3c7f1266dc07999a2eb102e5ab9f102c9f5e1f81
Denis Catruna <devengvengg@gmail.com>
2022-11-17 14:13:53 +0100

    Solucion Set Up

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// ------------------ @ My Code:
+Route::get('/encuentro', function () {
+    return view('encuentro');
+});
